cded0d6a396bc430a8179beee00c5a3605f76fd3
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2025-11-04 11:46:14 +0530

    fixed custoer issue

--- crm_pp/crm_pp/create_customer_from_lead.py
+++ crm_pp/crm_pp/create_customer_from_lead.py
@@ -1,18 +1,45 @@
+# import frappe
+# from erpnext.crm.doctype.lead.lead import _make_customer
+# @frappe.whitelist()
+# def create_customer_from_lead(doc, method=None):
+#     if doc.status == "Converted" or doc.status == "Convert":
+#         # frappe.msgprint("this is custom method====================")
+#         """
+#         Automatically create a Customer whenever a Lead is created.
+#         """
+#         try:
+#             existing_customer = frappe.db.exists("Customer", {"lead_name": doc.name})
+#             if existing_customer:
+#                 return
+
+#             customer_doc = _make_customer(source_name=doc.name)
+
+#             customer_doc.insert(ignore_permissions=True)
+#             frappe.db.commit()
+
+#             frappe.logger().info(f" Customer auto-created for Lead: {doc.name}")
+
+#         except Exception as e:
+#             frappe.log_error(f"Error auto-creating Customer from Lead {doc.name}: {str(e)}")
+
+
 import frappe
 from erpnext.crm.doctype.lead.lead import _make_customer
+
 @frappe.whitelist()
 def create_customer_from_lead(doc, method=None):
-    if doc.status == "Converted" or doc.status == "Convert":
-        # frappe.msgprint("this is custom method====================")
-        """
-        Automatically create a Customer whenever a Lead is created.
-        """
+    if doc.status in ["Converted", "Convert"]:
         try:
             existing_customer = frappe.db.exists("Customer", {"lead_name": doc.name})
             if existing_customer:
                 return
 
+            # create customer
             customer_doc = _make_customer(source_name=doc.name)
+
+            if hasattr(customer_doc, "customer_type"):
+                if customer_doc.customer_type == "Company":
+                    customer_doc.customer_type = "Business Owner"
 
             customer_doc.insert(ignore_permissions=True)
             frappe.db.commit()
@@ -20,4 +47,7 @@ def create_customer_from_lead(doc, method=None):
             frappe.logger().info(f" Customer auto-created for Lead: {doc.name}")
 
         except Exception as e:
-            frappe.log_error(f"Error auto-creating Customer from Lead {doc.name}: {str(e)}")
+            frappe.log_error(
+                f"Error auto-creating Customer from Lead {doc.name}: {str(e)}",
+                "Lead to Customer Auto Creation Error"
+            )
