0cc635f1c604d923a96b24012e5b3a3580ac9499
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2025-11-04 13:14:40 +0530

    fixed customer Issue

--- crm_pp/crm_pp/create_customer_from_lead.py
+++ crm_pp/crm_pp/create_customer_from_lead.py
@@ -9,16 +9,18 @@ def create_customer_from_lead(doc, method=None):
             if existing_customer:
                 return
 
-            # create customer
             customer_doc = _make_customer(source_name=doc.name)
 
             if hasattr(customer_doc, "customer_type"):
-                if customer_doc.customer_type == "Company":
+                if customer_doc.customer_type.strip() == "Company":
                     customer_doc.customer_type = "Business Owner "
 
+            # Insert without running validations
+            customer_doc.flags.ignore_validate = True
+            customer_doc.flags.ignore_mandatory = True
             customer_doc.insert(ignore_permissions=True)
-            frappe.db.commit()
 
+            frappe.db.commit()
             frappe.logger().info(f" Customer auto-created for Lead: {doc.name}")
 
         except Exception as e:
